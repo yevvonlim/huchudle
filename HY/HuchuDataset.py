41bd00e6919009aa4b63a10ad28dda9732bb4a2b
sunkite3-3 <83447421+sunkite3-3@users.noreply.github.com>
2024-05-13 14:44:02 -0400

    Update HuchuDataset.py

--- HY/HuchuDataset.py
+++ HY/HuchuDataset.py
@@ -3,49 +3,53 @@ from PIL import Image, ImageDraw
 import json
 import torch
 import torch.nn as nn
-from glob import glob
 from torch.utils.data import Dataset, DataLoader
-import os.path as osp
-import os
+import cv2
 
 from tqdm.auto import tqdm
 
+def member(data):
+    
+     # black backgraound 
+    draw_img = Image.new("RGB", (64, 64), color="black")
+    
+    draw = ImageDraw.Draw(draw_img)
+
+    for dot in data["landmark"]:
+        x, y = dot
+        ratio = 64/256
+        draw.point((x * ratio, y * ratio), fill="white")
+        
+    return draw_img
 
 class HuchuDataset(nn.Dataset):
 
-    def member(data):
-    
-         # black backgraound 
-        image = Image.new("RGB", (64, 64), color="black")
-    
-        draw = ImageDraw.Draw(image)
-
-        for dot in data["landmark"]:
-            x, y = dot
-            ratio = 64/256
-            draw.point((x * ratio, y * ratio), fill="white")
-        
-        return image
-    
+    def __init__(self, data_dir):
+        self.file_path = ("/workspace/huchudle/HY/dataset/dataset_list.json")
+   
     def __getitem__(self, idx):
-        
-        # open caption JSON file
+        #img, captions, draw_img 
+    
+        # open JSON file
         with open(self.file_path, "r") as json_file:
             data = json.load(json_file) 
+      
+        #image_id 받아서 img 가져오기 
+        img_id = data[idx]["image_id"]
+        img = cv2.imread('/workspace/huchudle/HY/dataset/cropped_img' + img_id)
 
-        # image_id 받아서 image 저장된 경로에서 가져오기 
-        img = data["image_id"] 
+        #caption
+        caption = data[idx]["caption"]
 
-        draw_img = self.member(data)
-
-        caption = data["caption"]
+        #landmark image
+        draw_img = member(data[idx])
 
         return img, draw_img, caption
     
 if __name__ == "__main__":
-    dataset = HuchuDataset('/workspace/train2017') # 데이터 경로
+    dataset = HuchuDataset('/workspace/huchudle/HY/dataset/cropped_img') # 이미지 데이터 경로 
     dataloader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=False)
-    # print(dataloader[0])
-    for i, (img, fname) in enumerate(tqdm(dataloader)):
-        assert (img.shape[1] == 3)
+
+
+
 
