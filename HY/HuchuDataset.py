1843444a62a0dfa7d8fc2019ea84baa2159432c2
sunkite3-3 <83447421+sunkite3-3@users.noreply.github.com>
2024-05-13 15:22:47 -0400

    Update HuchuDataset.py

--- HY/HuchuDataset.py
+++ HY/HuchuDataset.py
@@ -2,15 +2,16 @@ import dlib
 from PIL import Image, ImageDraw
 import json
 import torch
+import torchvision
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import cv2
 
 from tqdm.auto import tqdm
 
-def member(data):
+def draw_landmarks(data):
     
-     # black backgraound 
+    # black backgraound 
     draw_img = Image.new("RGB", (64, 64), color="black")
     
     draw = ImageDraw.Draw(draw_img)
@@ -22,33 +23,33 @@ def member(data):
         
     return draw_img
 
-class HuchuDataset(nn.Dataset):
+class HuchuDataset(Dataset):
 
-    def __init__(self, data_dir):
-        self.file_path = ("/workspace/huchudle/HY/dataset/dataset_list.json")
-   
-    def __getitem__(self, idx):
-        #img, captions, draw_img 
-    
+    def __init__(self, ann_path, root_dir):
+        self.file_path = ann_path
+        self.root_dir = root_dir
+
+    def __getitem__(self, idx):    
         # open JSON file
         with open(self.file_path, "r") as json_file:
             data = json.load(json_file) 
       
-        #image_id 받아서 img 가져오기 
+        # img
         img_id = data[idx]["image_id"]
-        img = cv2.imread('/workspace/huchudle/HY/dataset/cropped_img' + img_id)
+        img = cv2.imread(self.root_dir + img_id)
+       
 
-        #caption
+        # caption
         caption = data[idx]["caption"]
 
-        #landmark image
-        draw_img = member(data[idx])
+        # landmark image
+        draw_img = draw_landmarks(data[idx])
 
         return img, draw_img, caption
     
 if __name__ == "__main__":
-    dataset = HuchuDataset('/workspace/huchudle/HY/dataset/cropped_img') # 이미지 데이터 경로 
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=False, drop_last=False, pin_memory=False)
+    dataset = HuchuDataset('/workspace/huchudle/HY/dataset/dataset_list.json', '/workspace/huchudle/HY/dataset/cropped_img/') 
+    
 
 
 
